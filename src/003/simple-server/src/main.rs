328ca228fef844f02939edc4433f6d6cd01ed4d8
Joe Chen <geometryolife@gmail.com>
2022-12-12 20:07:16 +0800

    003: refactoring the simple web server

--- src/003/simple-server/src/main.rs
+++ src/003/simple-server/src/main.rs
@@ -9,21 +9,17 @@ fn handle_client(mut stream: TcpStream) {
 
     // Conditionally return to a web page
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        // Retrun the contents of `main.html`
-        let contents = fs::read_to_string("main.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "main.html")
     } else {
-        // Return the contents of `404.html`
-        let contents = fs::read_to_string("404.html").unwrap();
-        let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", contents);
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn main() -> std::io::Result<()> {
