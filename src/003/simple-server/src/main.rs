42fa531770ff09cbb098b0e47da733895da35d17
Joe Chen <geometryolife@gmail.com>
2022-12-12 17:36:25 +0800

    003: read the request contents

--- src/003/simple-server/src/main.rs
+++ src/003/simple-server/src/main.rs
@@ -1,17 +1,13 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
-
+use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(_stream: TcpStream) {
-    // ...
-    println!("Received some contents...");
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
 
 fn main() -> std::io::Result<()> {
-    // let listener = TcpListener::bind("127.0.0.1:80").unwrap();
-    // It is more convenient and concise to use `?`
     let listener = TcpListener::bind("127.0.0.1:8080")?; // `?` means that the returned type is Result
 
     // accept connections and process them serially
