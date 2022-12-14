8efe7a82dce6e50d56e61c6c7a066db206b99412
Joe Chen <geometryolife@gmail.com>
2022-12-14 22:34:02 +0800

    005: ping and pong

--- src/005/src/main.rs
+++ src/005/src/main.rs
@@ -1,6 +1,7 @@
 use libp2p::futures::StreamExt;
-use libp2p::swarm::{DummyBehaviour, Swarm, SwarmEvent};
-use libp2p::{identity, PeerId};
+use libp2p::ping::{Ping, PingConfig};
+use libp2p::swarm::{Swarm, SwarmEvent};
+use libp2p::{identity, Multiaddr, PeerId};
 use std::error::Error;
 
 #[tokio::main]
@@ -10,17 +11,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("Local Peer ID is: {:?}", new_peer_id);
 
-    let behaviour = DummyBehaviour::default();
     let transport = libp2p::development_transport(new_key).await?;
+    let behaviour = Ping::new(PingConfig::new().with_keep_alive(true));
     let mut swarm = Swarm::new(transport, behaviour, new_peer_id);
 
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    if let Some(remote_peer) = std::env::args().nth(1) {
+        let remote_peer_multiaddr: Multiaddr = remote_peer.parse()?;
+        swarm.dial(remote_peer_multiaddr)?;
+        println!("Dialed remote peer: {:?}", remote_peer);
+    }
 
     loop {
         match swarm.select_next_some().await {
             SwarmEvent::NewListenAddr { address, .. } => {
                 println!("Listening on Local Address {:?}", address)
             }
+            SwarmEvent::Behaviour(event) => println!("Event received from peer is {:?}", event),
             _ => {}
         }
     }
