f763cb0aac05c2d6f07fd7a360525cdec80f48f1
Joe Chen <geometryolife@gmail.com>
2022-12-18 22:58:28 +0800

    009: impl basic function of todo app

--- src/009/src/app.rs
+++ src/009/src/app.rs
@@ -1,15 +1,87 @@
-use simple_todo::todolist::Enum;
+use simple_todo::todolist::{Enum, TodoItem};
 
 #[derive(Default)]
 pub struct MyApp {
     addtodo: String,
     task: usize,
     radio: Enum,
+    data: Vec<TodoItem>,
+    del: i32,
 }
 
 impl MyApp {
     pub fn new(cc: &eframe::CreationContext) -> Self {
-        Self::default()
+        // Self::default()
+        let mut data = Self::default();
+        // if let Some(storage) = cc.storage {
+        //     data = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
+        // simple_todo::setup_custom_fonts(&cc.egui_ctx);
+        data.del = -1;
+        data
+    }
+
+    fn show_content(&mut self, ui: &mut eframe::egui::Ui) {
+        match self.radio {
+            Enum::All => {
+                self.show_all(ui);
+            }
+            Enum::Active => {
+                self.show_active(ui, false);
+            }
+            Enum::Completed => {
+                self.show_active(ui, true);
+            }
+        }
+    }
+
+    fn show_all(&mut self, ui: &mut eframe::egui::Ui) {
+        if self.data.len() > 0 {
+            for (idx, todoitem) in &mut self.data.iter_mut().enumerate() {
+                if todoitem.singleitem(ui) {
+                    self.del = idx as i32;
+                };
+            }
+        } else {
+            ui.label("You have not create task yet:ヾ(•ω•`)o");
+        }
+    }
+
+    fn show_active(&mut self, ui: &mut eframe::egui::Ui, active: bool) {
+        let datae = self
+            .data
+            .iter()
+            .filter(|u| u.active == active)
+            .collect::<Vec<_>>();
+
+        if datae.len() > 0 {
+            for (idx, todoitem) in &mut self.data.iter_mut().enumerate() {
+                if todoitem.active == active {
+                    if todoitem.singleitem(ui) {
+                        self.del = idx as i32;
+                    };
+                }
+            }
+        } else {
+            if active {
+                ui.label("You have not task todo yet!");
+            } else {
+                ui.label("You have not complete yet!");
+            }
+        }
+    }
+
+    fn dataupdate(&mut self) {
+        let datae = self
+            .data
+            .iter()
+            .filter(|u| u.active == false)
+            .collect::<Vec<_>>();
+        self.task = datae.len();
+        if self.del > -1 {
+            self.data.remove(self.del as usize);
+            self.del = -1;
+        }
     }
 }
 
@@ -17,10 +89,22 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
         eframe::egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My ToDo List");
-            ui.add(
+            let p = ui.add(
                 eframe::egui::TextEdit::singleline(&mut self.addtodo)
                     .hint_text("Write something here"),
             );
+            if p.lost_focus() & !p.clicked_elsewhere() {
+                if self.addtodo.is_empty() {
+                } else {
+                    self.data.push(TodoItem {
+                        title: self.addtodo.clone(),
+                        active: false,
+                        edit: false,
+                    });
+
+                    self.addtodo.clear();
+                }
+            }
             ui.horizontal(|ui| {
                 ui.label(&self.task.to_string());
                 ui.label("task left");
@@ -30,6 +114,9 @@ impl eframe::App for MyApp {
                 ui.selectable_value(&mut self.radio, Enum::Active, "Active");
                 ui.selectable_value(&mut self.radio, Enum::Completed, "Completed");
             });
+            ui.add_space(11.0);
+            self.show_content(ui);
         });
+        self.dataupdate();
     }
 }
--- src/009/src/todolist.rs
+++ src/009/src/todolist.rs
@@ -5,3 +5,34 @@ pub enum Enum {
     Active,
     Completed,
 }
+
+pub struct TodoItem {
+    pub title: String,
+    pub active: bool,
+    pub edit: bool,
+}
+
+impl TodoItem {
+    pub fn singleitem(&mut self, ui: &mut eframe::egui::Ui) -> bool {
+        let mut index = false;
+        ui.horizontal(|ui| {
+            if self.edit {
+                let respon = ui.text_edit_singleline(&mut self.title);
+                if respon.lost_focus() || respon.clicked_elsewhere() {
+                    self.edit = false;
+                }
+                respon.request_focus();
+
+                if ui.button("Delete").clicked() {
+                    index = true;
+                };
+            } else {
+                ui.checkbox(&mut self.active, &self.title);
+                if ui.button("🖊").clicked(){ 
+                    self.edit = true;
+                };
+            }
+        });
+        index
+    }
+}
